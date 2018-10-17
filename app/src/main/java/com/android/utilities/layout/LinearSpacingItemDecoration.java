993c05e6d9d9d71bfd6cb50c5645273fd36de6dd
Ellie Tam <ellie.y.tam@accenture.com>
2018-10-17 16:50:38 +0800

    Update spacing for recycler view

--- app/src/main/java/com/android/utilities/layout/LinearSpacingItemDecoration.java
+++ app/src/main/java/com/android/utilities/layout/LinearSpacingItemDecoration.java
@@ -13,18 +13,16 @@ public class LinearSpacingItemDecoration extends RecyclerView.ItemDecoration {
     }
 
     @Override
-    public void getItemOffsets(Rect outRect, View view,
+    public void getItemOffsets(Rect outRect,
+                               View view,
                                RecyclerView parent,
                                RecyclerView.State state) {
+
+        final int position = parent.getChildLayoutPosition(view);
+
         outRect.left = space;
         outRect.right = space;
+        outRect.top = (position == 0) ? space : 0;
         outRect.bottom = space;
-
-        // Add top margin only for the first item to avoid double space between items
-        if (parent.getChildLayoutPosition(view) == 0) {
-            outRect.top = space;
-        } else {
-            outRect.top = 0;
-        }
     }
 }
