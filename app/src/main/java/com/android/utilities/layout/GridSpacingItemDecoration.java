5eeac1793fec2ff0e3e2ac2873e771172bebbf1a
Ellie Tam <ellie.y.tam@accenture.com>
2018-10-17 16:34:03 +0800

    FIXED the size of your RecyclerView item in all devices

---
+++ app/src/main/java/com/android/utilities/layout/GridSpacingItemDecoration.java
@@ -0,0 +1,32 @@
+package com.android.utilities.layout;
+
+import android.graphics.Rect;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
+
+    private int mSpanCount;
+    private float mItemSize;
+
+    public GridSpacingItemDecoration(int spanCount, int itemSize) {
+        this.mSpanCount = spanCount;
+        mItemSize = itemSize;
+    }
+
+    @Override
+    public void getItemOffsets(final Rect outRect, final View view, RecyclerView parent,
+                               RecyclerView.State state) {
+        final int position = parent.getChildLayoutPosition(view);
+        final int column = position % mSpanCount;
+        final int parentWidth = parent.getWidth();
+        int spacing = (int) (parentWidth - (mItemSize * mSpanCount)) / (mSpanCount + 1);
+        outRect.left = spacing - column * spacing / mSpanCount;
+        outRect.right = (column + 1) * spacing / mSpanCount;
+
+        if (position < mSpanCount) {
+            outRect.top = spacing;
+        }
+        outRect.bottom = spacing;
+    }
+}
--- app/src/main/java/com/android/utilities/layout/LinearSpacingItemDecoration.java
+++ app/src/main/java/com/android/utilities/layout/LinearSpacingItemDecoration.java
@@ -4,16 +4,18 @@ import android.graphics.Rect;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
-public class GridSpacesItemDecoration extends RecyclerView.ItemDecoration {
+public class LinearSpacingItemDecoration extends RecyclerView.ItemDecoration {
+
     private int space;
 
-    public GridSpacesItemDecoration(int space) {
+    public LinearSpacingItemDecoration(int space) {
         this.space = space;
     }
 
     @Override
     public void getItemOffsets(Rect outRect, View view,
-                               RecyclerView parent, RecyclerView.State state) {
+                               RecyclerView parent,
+                               RecyclerView.State state) {
         outRect.left = space;
         outRect.right = space;
         outRect.bottom = space;
