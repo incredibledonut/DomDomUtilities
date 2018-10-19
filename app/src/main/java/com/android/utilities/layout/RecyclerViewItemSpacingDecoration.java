e5d7113431c629fe1d4e887622a07c9192d9884b
Ellie Tam <ellie.y.tam@accenture.com>
2018-10-19 22:39:13 +0800

    RecyclerViewItemSpacingDecoration update

--- app/src/main/java/com/android/utilities/layout/RecyclerViewItemSpacingDecoration.java
+++ app/src/main/java/com/android/utilities/layout/RecyclerViewItemSpacingDecoration.java
@@ -4,13 +4,13 @@ import android.graphics.Rect;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
-public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
+public class RecyclerViewItemSpacingDecoration extends RecyclerView.ItemDecoration {
 
     private int spanCount;
     private int spacing;
     private boolean includeEdge;
 
-    public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
+    public RecyclerViewItemSpacingDecoration(int spanCount, int spacing, boolean includeEdge) {
         this.spanCount = spanCount;
         this.spacing = spacing;
         this.includeEdge = includeEdge;
