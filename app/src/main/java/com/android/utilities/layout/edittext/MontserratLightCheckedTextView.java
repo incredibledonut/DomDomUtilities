597abaf190ff0d25f021aabaa4fd10513885868e
DESKTOP-JR3G0DK\Dominic <incredibledonut@gmail.com>
2018-10-20 23:44:35 +0800

    add montserratlight checked textview

---
+++ app/src/main/java/com/android/utilities/layout/edittext/MontserratLightCheckedTextView.java
@@ -0,0 +1,29 @@
+package com.android.utilities.layout.edittext;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.support.v7.widget.AppCompatCheckedTextView;
+import android.util.AttributeSet;
+
+public class MontserratLightCheckedTextView extends AppCompatCheckedTextView {
+
+    public MontserratLightCheckedTextView(Context context) {
+        super(context);
+        setupTypeface(context);
+    }
+
+    public MontserratLightCheckedTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setupTypeface(context);
+    }
+
+    public MontserratLightCheckedTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setupTypeface(context);
+    }
+
+    private void setupTypeface(Context context) {
+        setTypeface(Typeface.createFromAsset(context.getAssets(), "fonts/Montserrat-Light.ttf"));
+    }
+
+}
